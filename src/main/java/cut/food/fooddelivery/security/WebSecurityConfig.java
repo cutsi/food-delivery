507b37ea04ac78606c26bd39d47e400715a23aa0
Josip Čutura <cutsi>
2021-11-08 11:22:08 +0100

    fixing index page 105

--- src/main/java/cut/food/fooddelivery/security/WebSecurityConfig.java
+++ src/main/java/cut/food/fooddelivery/security/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/**","/index","/getCat","/saveCat")
+                .antMatchers("/**","/style.css","/index","/getCat","/saveCat")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
