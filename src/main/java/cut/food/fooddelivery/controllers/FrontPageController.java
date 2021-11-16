e8b8cf2eb37756469062c43203d3572f312a03c2
Josip Čutura <cutsi>
2021-11-16 15:09:24 +0100

    fixing index page 189

--- src/main/java/cut/food/fooddelivery/controllers/FrontPageController.java
+++ src/main/java/cut/food/fooddelivery/controllers/FrontPageController.java
@@ -38,4 +38,8 @@ public class FrontPageController {
         }
         return "index";
     }
+    @GetMapping("registracija")
+    public String registracija(){
+        return "signup";
+    }
 }
