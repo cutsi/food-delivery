5a753f6cfd82b0b5b3ea45a76129a6e090d2245f
Josip Čutura <cutsi>
2021-11-19 22:13:14 +0100

    fixing index page 292

--- src/main/resources/static/js/flickity.pkgd.js
+++ src/main/resources/static/js/flickity.pkgd.js
@@ -3313,7 +3313,98 @@
 
 }( window, function factory( window, Flickity, Unipointer, utils ) {
 
+// -------------------------- PageDots -------------------------- //
 
+
+
+    function PageDots( parent ) {
+        this.parent = parent;
+        this._create();
+    }
+
+    PageDots.prototype = Object.create( Unipointer.prototype );
+
+    PageDots.prototype._create = function() {
+        // create holder element
+        this.holder = document.createElement('ol');
+        this.holder.className = 'flickity-page-dots';
+        // create dots, array of elements
+        this.dots = [];
+        // events
+        this.handleClick = this.onClick.bind( this );
+        this.on( 'pointerDown', this.parent.childUIPointerDown.bind( this.parent ) );
+    };
+
+    PageDots.prototype.activate = function() {
+        this.setDots();
+        this.holder.addEventListener( 'click', this.handleClick );
+        this.bindStartEvent( this.holder );
+        // add to DOM
+        this.parent.element.appendChild( this.holder );
+    };
+
+    PageDots.prototype.deactivate = function() {
+        this.holder.removeEventListener( 'click', this.handleClick );
+        this.unbindStartEvent( this.holder );
+        // remove from DOM
+        this.parent.element.removeChild( this.holder );
+    };
+
+    PageDots.prototype.setDots = function() {
+        // get difference between number of slides and number of dots
+        var delta = this.parent.slides.length - this.dots.length;
+        if ( delta > 0 ) {
+            this.addDots( delta );
+        } else if ( delta < 0 ) {
+            this.removeDots( -delta );
+        }
+    };
+
+
+
+    PageDots.prototype.removeDots = function( count ) {
+        // remove from this.dots collection
+        var removeDots = this.dots.splice( this.dots.length - count, count );
+        // remove from DOM
+        removeDots.forEach( function( dot ) {
+            this.holder.removeChild( dot );
+        }, this );
+    };
+
+    PageDots.prototype.updateSelected = function() {
+        // remove selected class on previous
+        if ( this.selectedDot ) {
+            this.selectedDot.className = 'dot';
+            this.selectedDot.removeAttribute('aria-current');
+        }
+        // don't proceed if no dots
+        if ( !this.dots.length ) {
+            return;
+        }
+        this.selectedDot = this.dots[ this.parent.selectedIndex ];
+        this.selectedDot.className = 'dot is-selected';
+        this.selectedDot.setAttribute( 'aria-current', 'step' );
+    };
+
+    PageDots.prototype.onTap = // old method name, backwards-compatible
+        PageDots.prototype.onClick = function( event ) {
+            var target = event.target;
+            // only care about dot clicks
+            if ( target.nodeName != 'LI' ) {
+                return;
+            }
+
+            this.parent.uiChange();
+            var index = this.dots.indexOf( target );
+            this.parent.select( index );
+        };
+
+    PageDots.prototype.destroy = function() {
+        this.deactivate();
+        this.allOff();
+    };
+
+    Flickity.PageDots = PageDots;
 
 // -------------------------- Flickity -------------------------- //
 
