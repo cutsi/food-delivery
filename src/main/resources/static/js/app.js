f307b5c43ea8555731c85f11fcd49c35c23d5217
Josip Čutura <cutsi>
2021-12-06 19:19:00 +0100

    fixing index page 485

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,7 +1,1 @@
-const accordion = document.getElementsByClassName('container');
-
-for (i=0; i<accordion.length; i++) {
-    accordion[i].addEventListener('click', function () {
-        this.classList.toggle('active')
-    })
-}
+$('.collapse-title').click(function() {   $('.collapse-details').toggle(); })
