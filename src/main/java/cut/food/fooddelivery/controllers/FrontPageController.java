ad6b6e7cfbf17d9b05ea68f3d0b406bb2dabafe9
Josip Čutura <cutsi>
2021-12-29 01:47:43 +0100

    fixing index page 627

--- src/main/java/cut/food/fooddelivery/controllers/FrontPageController.java
+++ src/main/java/cut/food/fooddelivery/controllers/FrontPageController.java
@@ -66,7 +66,7 @@ public class FrontPageController {
              ) {
             System.out.println("NAME OF THE FOOD ITEM " + f.getName());
         }
-        model.addAttribute("foodItems",foodItemList);
+        model.addAttribute("foodItems",restaurant.getMenu());
         model.addAttribute("restaurant", restaurant);
         return "restaurant";
     }
