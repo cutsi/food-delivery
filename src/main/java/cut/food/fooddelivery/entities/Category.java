9809be8da94e5827207d8a28093621b520ce2a5f
Josip Čutura <cutsi>
2022-01-02 17:08:28 +0100

    fixing index page 681

--- src/main/java/cut/food/fooddelivery/entities/Category.java
+++ src/main/java/cut/food/fooddelivery/entities/Category.java
@@ -27,6 +27,7 @@ public class Category {
     )
     private Long id;
     private String categoryName;
+    private String image;
     @OneToMany(mappedBy = "category")
     private Set<FoodItem> foodItems = new HashSet<>();
     public Category(String categoryName){
