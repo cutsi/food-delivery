8043e241fddc27ec2739d08788e54ad88a14fac9
Josip Čutura <cutsi>
2022-01-08 23:49:18 +0100

    fixing restaurant page 747

--- src/main/java/cut/food/fooddelivery/entities/PortionSize.java
+++ src/main/java/cut/food/fooddelivery/entities/PortionSize.java
@@ -1,4 +1,28 @@
 package cut.food.fooddelivery.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
 public class PortionSize {
+    @SequenceGenerator(
+            name = "portionSize_sequence",
+            sequenceName = "portionSize_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "portionSize_sequence"
+    )
+    private Long id;
+    private String size;
 }
