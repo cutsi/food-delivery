c5648155683a3ba62b0a9290df18e2b3bced214d
Josip Čutura <cutsi>
2022-01-11 19:00:45 +0100

    fixing restaurant page 792

--- src/main/java/cut/food/fooddelivery/entities/PortionSize.java
+++ src/main/java/cut/food/fooddelivery/entities/PortionSize.java
@@ -28,6 +28,7 @@ public class PortionSize {
     private Long id;
     private String size;
     private Boolean isChecked;
+
     @ManyToMany
     @JoinTable(
             name = "portionSize_foodItem",
